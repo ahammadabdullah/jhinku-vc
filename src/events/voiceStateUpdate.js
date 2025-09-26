baedeb5a359e112959b15476566b6582e7a6554e
Ahammad Abdullah <ahammadabdullahnew@gmail.com>
2025-09-26 20:16:15 +0600

    fix: channel name

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,91 +1,91 @@
-import { ChannelType } from 'discord.js'
-import { log } from '../utils/logger.js'
-import t from '../utils/t.js'
-import config from '../../config/config.js'
+import { ChannelType } from "discord.js";
+import { log } from "../utils/logger.js";
+import t from "../utils/t.js";
+import config from "../../config/config.js";
 
-const loggedDeleted = new Set()
+const loggedDeleted = new Set();
 
 export default async (client, oldState, newState) => {
-  const oldChannel = oldState.channel
-  const newChannel = newState.channel
-  const member = newState.member || oldState.member
-  const lang = config.language
+  const oldChannel = oldState.channel;
+  const newChannel = newState.channel;
+  const member = newState.member || oldState.member;
+  const lang = config.language;
 
   if (!oldChannel && newChannel) {
     if (newChannel.id === process.env.VOICE_CHANNEL_ID) {
       const temp = await newChannel.guild.channels.create({
-        name: `${member.user.username} - room`,
+        name: `${member.user.username}'s - room`,
         type: ChannelType.GuildVoice,
-        parent: process.env.CATEGORY_CHANNEL_ID
-      })
+        parent: process.env.CATEGORY_CHANNEL_ID,
+      });
 
-      await newState.setChannel(temp)
-      client.tempVoiceOwners ??= new Map()
-      client.tempVoiceOwners.set(temp.id, member.id)
+      await newState.setChannel(temp);
+      client.tempVoiceOwners ??= new Map();
+      client.tempVoiceOwners.set(temp.id, member.id);
 
-      log('log_switched', client, {
+      log("log_switched", client, {
         user: member.user.username,
         from: newChannel.name,
-        to: temp.name
-      })
+        to: temp.name,
+      });
     } else {
-      log('log_joined', client, {
+      log("log_joined", client, {
         user: member.user.username,
-        channel: newChannel.name
-      })
+        channel: newChannel.name,
+      });
     }
 
-    return
+    return;
   }
 
   if (oldChannel && !newChannel) {
-    log('log_left', client, {
+    log("log_left", client, {
       user: member.user.username,
-      channel: oldChannel.name
-    })
+      channel: oldChannel.name,
+    });
 
-    const isOwner = client.tempVoiceOwners?.get(oldChannel.id) === member.id
-    if (oldChannel.members.size > 0 || !isOwner) return
+    const isOwner = client.tempVoiceOwners?.get(oldChannel.id) === member.id;
+    if (oldChannel.members.size > 0 || !isOwner) return;
 
-    await deleteChannel(oldChannel, client)
-    return
+    await deleteChannel(oldChannel, client);
+    return;
   }
 
   if (oldChannel && newChannel && oldChannel.id !== newChannel.id) {
-    const isOwner = client.tempVoiceOwners?.get(oldChannel.id) === member.id
-    if (oldChannel.members.size > 0 || !isOwner) return
+    const isOwner = client.tempVoiceOwners?.get(oldChannel.id) === member.id;
+    if (oldChannel.members.size > 0 || !isOwner) return;
 
-    await deleteChannel(oldChannel, client)
+    await deleteChannel(oldChannel, client);
   }
-}
+};
 
 async function deleteChannel(channel, client) {
-  if (!client.channels.cache.has(channel.id)) return
+  if (!client.channels.cache.has(channel.id)) return;
 
   try {
-    await channel.delete()
+    await channel.delete();
 
     if (
       !client.deletedByInteraction?.has(channel.id) &&
       !loggedDeleted.has(channel.id)
     ) {
-      log('log_deleted', client, { channel: channel.name })
-      loggedDeleted.add(channel.id)
+      log("log_deleted", client, { channel: channel.name });
+      loggedDeleted.add(channel.id);
     }
 
-    client.tempVoiceOwners.delete(channel.id)
-    client.deletedByInteraction?.delete(channel.id)
+    client.tempVoiceOwners.delete(channel.id);
+    client.deletedByInteraction?.delete(channel.id);
   } catch (err) {
     if (err.code === 10003) {
       if (
         !client.deletedByInteraction?.has(channel.id) &&
         !loggedDeleted.has(channel.id)
       ) {
-        log('log_deleted', client, { channel: channel.name })
-        loggedDeleted.add(channel.id)
+        log("log_deleted", client, { channel: channel.name });
+        loggedDeleted.add(channel.id);
       }
     } else {
-      log('log_channel_delete_failed', client, { channel: channel.name })
+      log("log_channel_delete_failed", client, { channel: channel.name });
     }
   }
 }
